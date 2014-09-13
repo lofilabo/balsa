fea5c5aeadc21366e873510b37a3eb17aebf1c1f
richardlofi <richardlofi@gmail.com>
2014-09-13 23:43:59 +0900

    gitignore

--- Y_CONFIG/database.php
+++ Y_CONFIG/database.php
@@ -4,24 +4,13 @@ require 'vendor/autoload.php';
 use Illuminate\Database\Capsule\Manager as Capsule;  
  
 $capsule = new Capsule; 
- /*
-$capsule->addConnection(array(
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'u076oran_kanri',
-    'username'  => 'u076oran_wpuser',
-    'password'  => 'dy9Otz109',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => ''
-));
- */
+
  $capsule->addConnection(array(
     'driver'    => 'mysql',
     'host'      => 'localhost',
-    'database'  => 'u076oran_kanri',
-    'username'  => 'richard',
-    'password'  => 'richard',
+    'database'  => '',
+    'username'  => '',
+    'password'  => '',
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => ''
